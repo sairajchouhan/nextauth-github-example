0d65b7480deec398efff0fad4dec64c7ec36f441
sairaj <sairaj2119@gmail.com>
2022-03-08 22:25:32 +0530

    finish

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { signIn, useSession, signOut } from 'next-auth/react'
 import Link from 'next/link'
 import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 
 const Header = () => {
+  const { data, status } = useSession()
+
+  const handleSignIn = async () => {
+    await signIn('github', {
+      callbackUrl: 'http://localhost:3000/dashboard',
+    })
+  }
+
+  const handleLogout = async () => {
+    await signOut({
+      callbackUrl: 'http://localhost:3000/',
+    })
+  }
+
   return (
     <Navbar bg="light">
       <Container>
@@ -11,9 +26,31 @@ const Header = () => {
         </Link>
         <Navbar.Collapse className="justify-content-end">
           <Nav className="ml-auto">
-            <Nav.Link as="div">
-              <Button>SignIn</Button>
-            </Nav.Link>
+            {status === 'authenticated' ? (
+              <>
+                {data.user?.image && data.user.name ? (
+                  <img
+                    src={data.user.image}
+                    alt={data.user.name}
+                    width={50}
+                    height={50}
+                    style={{ borderRadius: '50%' }}
+                  />
+                ) : null}
+                <Nav.Link as="div">
+                  <Link href="/dashboard" passHref>
+                    <Button variant="outline-primary">Dashboard</Button>
+                  </Link>
+                </Nav.Link>
+                <Nav.Link as="div">
+                  <Button onClick={handleLogout}>Logout</Button>
+                </Nav.Link>
+              </>
+            ) : (
+              <Nav.Link as="div">
+                <Button onClick={handleSignIn}>SignIn</Button>
+              </Nav.Link>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
 import Header from '../components/Header'
+import { SessionProvider } from 'next-auth/react'
+import AuthWrapper from '../components/AuthWrapper'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
+    <SessionProvider session={pageProps.session}>
+      <AuthWrapper>
+        <Header />
+        <Component {...pageProps} />
+      </AuthWrapper>
+    </SessionProvider>
   )
 }
 
